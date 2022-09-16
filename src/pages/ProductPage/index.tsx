ef8923f2fe2f56df489a607ec6c5c0c454ec0ca0
Marko <drakulic.marko0@gmail.com>
2022-09-16 13:04:24 +0200

    Added large screen product page

--- src/pages/ProductPage/index.tsx
+++ src/pages/ProductPage/index.tsx
@@ -31,6 +31,7 @@ const ProductPage = () => {
     },
     {
       refetchOnWindowFocus: false,
+      refetchOnMount: false,
     }
   );
   if (isLoading || isRefetching)
@@ -57,26 +58,46 @@ const ProductPage = () => {
     );
 
   return (
-    <main className="mx-4 lg:mx-auto lg:max-w-[1248px]">
-      <div className="mb-4" />
+    <main className="mx-4 lg:mx-auto lg:max-w-[1248px] lg:grid lg:grid-cols-2">
+      <div className="mb-4 lg:hidden" />
       <img
         src={product?.image?.url}
         alt={product?.name}
-        className="h-[374px]"
+        className="h-[374px] lg:h-[600px] object-cover"
       />
-      <div className="mb-6" />
-      <h1 className="text-xl leading-[26px]">{product?.name}</h1>
-      <div className="mb-[5px]0" />
-      <span className="text-accent">{product?.price.formatted_with_code}</span>
-      <div className="mb-6" />
-      <p className="text-sm text-darkGray leading-5">
-        {product?.description.replace(/<(.|\n)*?>/g, "")}
-      </p>
-      <div className="mb-6" />
-      <button className="bg-white text-black py-[6px] w-full rounded-md border border-black uppercase hover:bg-black hover:text-white transition-colors duration-150">
-        Add to cart
-      </button>
-      <div className="mb-24" />
+      <div className="">
+        <div className="mb-6 lg:hidden" />
+        <h1 className="text-xl leading-[26px] lg:text-[26px] lg:leading-[35px]">
+          {product?.name}
+        </h1>
+        <div className="mb-[5px] lg:mb-6" />
+        <span className="text-accent lg:text-xl lg:leading-5">
+          {product?.price.formatted_with_code}
+        </span>
+        <div className="mb-6" />
+        <p className="text-sm text-darkGray leading-5 lg:text-base lg:min-h-[200px]">
+          {product?.description.replace(/<(.|\n)*?>/g, "")}
+        </p>
+        <div className="mb-6" />
+        <button className="bg-white text-black py-[6px] w-full rounded-md border border-black uppercase hover:bg-black hover:text-white transition-colors duration-150">
+          Add to cart
+        </button>
+
+        <div className="mb-6 lg:flex-grow" />
+        <div className="hidden lg:flex flex-col">
+          <span>
+            SKU: <span className="text-darkGray">{product?.sku}</span>
+          </span>
+          <span>
+            Categories:{" "}
+            <span className="text-darkGray">
+              {product?.categories.map((category) => category.name + " ")}
+            </span>
+          </span>
+        </div>
+
+        <div className="mb-24" />
+      </div>
     </main>
   );
 };
