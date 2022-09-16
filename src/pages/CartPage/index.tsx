a6f19457a5103b030273628e22a4321e3c4dc5a2
Marko <drakulic.marko0@gmail.com>
2022-09-16 14:21:14 +0200

    Large screens cart page added

--- src/pages/CartPage/index.tsx
+++ src/pages/CartPage/index.tsx
@@ -16,6 +16,7 @@ const CartPage = () => {
     },
     {
       refetchOnWindowFocus: false,
+      refetchOnMount: false,
     }
   );
 
@@ -26,20 +27,34 @@ const CartPage = () => {
       </div>
     );
   return (
-    <main className="mx-4 lg:mx-auto lg:max-w-[1248px] lg:grid lg:grid-cols-2">
-      <h1 className="leading-7">Shopping Cart</h1>
-      <div className="mb-4" />
-      {cart?.line_items.map((item) => (
-        <React.Fragment key={item.id}>
-          <CartItem {...item} />
-          <div className="mb-4" />
-        </React.Fragment>
-      ))}
-      <div className="mb-4" />
-      <button className="w-full text-xl border border-black rounded-[4px] px-[.75em] py-[.25em] disabled:cursor-not-allowed disabled:text-darkGray disabled:border-darkGray">
-        Checkout
-      </button>
-      <div className="mb-9" />
+    <main className="mx-4 lg:mx-auto lg:max-w-[1248px] lg:grid lg:grid-cols-2 lg:gap-20">
+      <h1 className="leading-7 col-span-2 lg:text-[33px] lg:text-center">
+        Shopping Cart
+      </h1>
+      <div className="lg:mb-4">
+        <div className="mb-4" />
+        {cart?.line_items.map((item) => (
+          <React.Fragment key={item.id}>
+            <CartItem {...item} />
+            <div className="mb-4" />
+          </React.Fragment>
+        ))}
+        <div className="mb-4" />
+        <button className="lg:hidden w-full text-xl border border-black rounded-[4px] px-[.75em] py-[.25em] disabled:cursor-not-allowed disabled:text-darkGray disabled:border-darkGray">
+          Checkout
+        </button>
+        <div className="mb-9" />
+      </div>
+      <div className="hidden lg:block text-2xl">
+        Total:{" "}
+        <span className="font-bold">
+          {cart?.subtotal.formatted_with_symbol}
+        </span>
+        <div className="mb-4" />
+        <button className="lg:block w-full text-xl border border-black rounded-[4px] px-[.75em] py-[.25em] disabled:cursor-not-allowed disabled:text-darkGray disabled:border-darkGray">
+          Checkout
+        </button>
+      </div>
     </main>
   );
 };
