238650475d7a14f23d5810797146f3ac513a0abf
Marko <drakulic.marko0@gmail.com>
2022-10-11 19:57:24 +0200

    Added a dialog popup on checkout

--- src/pages/CartPage/index.tsx
+++ src/pages/CartPage/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import { useQuery } from "react-query";
 import { CartItem } from "../../components";
 import commerce from "../../lib/commerce";
-
+import { Dialog, Transition } from "@headlessui/react";
 const CartPage = () => {
   const {
     data: cart,
@@ -19,7 +19,7 @@ const CartPage = () => {
       refetchOnMount: true,
     }
   );
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
   if (isLoading || isRefetching || isFetching)
     return (
       <div className="grid place-content-center">
@@ -51,10 +51,71 @@ const CartPage = () => {
           {cart?.subtotal.formatted_with_symbol}
         </span>
         <div className="mb-4" />
-        <button className="lg:block w-full text-xl border border-black rounded-[4px] px-[.75em] py-[.25em] disabled:cursor-not-allowed disabled:text-darkGray disabled:border-darkGray">
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="lg:block w-full text-xl border border-black rounded-[4px] px-[.75em] py-[.25em] disabled:cursor-not-allowed disabled:text-darkGray disabled:border-darkGray hover:bg-black hover:text-white transition-colors duration-150">
           Checkout
         </button>
       </div>
+      <Transition appear show={isModalOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => setIsModalOpen(false)}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0">
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95">
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900">
+                    Thank you for using my website!
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      This website is a mock e-commerce website. It is not
+                      connected to any payment gateway. If you wish to check out
+                      some of my other projects, please visit my{" "}
+                      <a
+                        href="https://github.com/Citrusthecrazy?tab=repositories"
+                        className="text-accent text-bold">
+                        GitHub
+                      </a>
+                      !
+                    </p>
+                  </div>
+
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-accent px-4 py-2 text-sm font-medium text-white hover:bg-accent/70 focus:outline-none focus-visible:ring-2 focus-visible:ring-accent focus-visible:ring-offset-2"
+                      onClick={() => setIsModalOpen(false)}>
+                      Got it, thanks!
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </main>
   );
 };
